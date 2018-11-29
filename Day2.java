02669ec30dc7191734ca104677d3b6a9b8cb82fe
dhruvkalra <dhruvkalra@hotmail.com>
2018-11-29 12:14:06 -0500

    Update Day2.java

--- Day2.java
+++ Day2.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class Day2{
 	public static void main(String[] args){
 		System.out.println("Praneet");
@@ -7,10 +8,21 @@ public class Day2{
 		System.out.println("Dhruv");
 		System.out.println("Hi");
 		int k=0;
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(8);
+		list.add(9);
+		list.add(5);
+		list.add(0);
+		System.out.println(method2(list));
 
 	}
-
-
-
-
+	public static int method2(ArrayList<Integer> a){
+		int b=0;
+		for(int i=0;i<a.size();i++){
+			b+=a.get(i);
+			b*=10;
+		}
+		b/=10;
+		return b;
+	}
 }
