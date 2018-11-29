992e530deee32026d7400b970bc214070bdd3279
praneetk07 <praneetk07@gmail.com>
2018-11-29 12:07:14 -0500

    starting the convertToArrayList method
    starting the first method

--- Day2.java
+++ Day2.java
@@ -1,4 +1,11 @@
+import java.util.ArrayList<>;
 public class Day2{
+	public static ArrayList<Integer> convertToArrayList(int x){
+
+
+
+	}
+
 	public static void main(String[] args){
 		System.out.println("Praneet");
 		System.out.println("Dhruv");
