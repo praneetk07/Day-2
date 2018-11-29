60324c71bd5f127c7cfe502dab01749980fe99a4
praneetk07 <praneetk07@gmail.com>
2018-11-29 12:14:24 -0500

    finished convertToArrayList method
    finished the first method and imported the arrayList class forom java

--- Day2.java
+++ Day2.java
@@ -1,7 +1,16 @@
-import java.util.ArrayList<>;
+import java.util.ArrayList;
 public class Day2{
 	public static ArrayList<Integer> convertToArrayList(int x){
+		ArrayList<Integer> a = new ArrayList<>();
+		int b = x;
+		while (b/10!=0 || b%10!=0){
+			int y = b%10;
+			b = b/10;
+			a.add(0,y);
 
+
+		}
+		return a;
 
 
 	}
@@ -13,7 +22,15 @@ public class Day2{
 
 		System.out.println("Dhruv");
 		System.out.println("Hi");
-		int k=0;
+		int k= 4086;
+		System.out.println(convertToArrayList(k));
+
+
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
 
 	}
 
